502100aaf72928ca13fc1de83aeaf48640a11b57
aveDN <117298308+aveDN@users.noreply.github.com>
2023-04-22 21:49:23 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,22 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remainIfAmount() {
+    public void remainIfAmount() {
         CashbackHackService cashbackHackService= new CashbackHackService();
-        int amount=1000;
-
+        int amount=900;
+        
         int actual= cashbackHackService.remain(amount);
-        int expected=0;
-        assertEquals(expected, actual);
+        int expected=100;
+        assertEquals(actual,expected);
     }
     @Test
     void shouldRemainToCashback1() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1001;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-        assertEquals(expected,actual);
+         CashbackHackService cashbackHackService= new CashbackHackService();
+        int amount=1100;
+        
+        int actual= cashbackHackService.remain(amount);
+        int expected=900;
+        assertEquals(actual,expected);
     }
     @Test
     void shouldRemainToCashback2() {
