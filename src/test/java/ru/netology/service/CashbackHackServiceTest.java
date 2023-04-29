5663870852dde14274f8f008a3e0d4ebe0285a6a
aveDN <117298308+aveDN@users.noreply.github.com>
2023-04-29 23:06:31 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,12 +8,11 @@ class CashbackHackServiceTest {
 
     @Test
     void remainIfAmount() {
-        CashbackHackService cashbackHackService= new CashbackHackService();
-        int amount=1000;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
 
-        int actual= cashbackHackService.remain(amount);
-        int expected=0;
-        assertEquals(expected, actual);
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
     }
     @Test
     void shouldRemainToCashback1() {
